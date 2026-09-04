585339cc4e52cad68b822ce073595b9697762fef
Anahi Faundez <afaundez@ipross.rionegro.gov.ar>
2026-09-04 09:53:09 -0300

    Primera entrega - Pantalla Perfil React Native

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,98 +1,67 @@
-import * as Device from 'expo-device';
-import { Platform, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { useState } from "react";
+import { Image, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 
-import { AnimatedIcon } from '@/components/animated-icon';
-import { HintRow } from '@/components/hint-row';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { WebBadge } from '@/components/web-badge';
-import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
+import { SafeAreaView } from "react-native-safe-area-context";
 
-function getDevMenuHint() {
-  if (Platform.OS === 'web') {
-    return <ThemedText type="small">use browser devtools</ThemedText>;
-  }
-  if (Device.isDevice) {
-    return (
-      <ThemedText type="small">
-        shake device or press <ThemedText type="code">m</ThemedText> in terminal
-      </ThemedText>
-    );
-  }
-  const shortcut = Platform.OS === 'android' ? 'cmd+m (or ctrl+m)' : 'cmd+d';
+export default function Index() {
+  const [nombre, setNombre] = useState(" Anahi")
+  const [apellido, setApellido] = useState("Faundez");
+  const [mail, setMail] = useState("Ana@gmail.com")
+  const [bio, setBio] = useState("Estudiante de programación apasionada por React Native y el desarrollo móvil.");
   return (
-    <ThemedText type="small">
-      press <ThemedText type="code">{shortcut}</ThemedText>
-    </ThemedText>
-  );
-}
-
-export default function HomeScreen() {
-  return (
-    <ThemedView style={styles.container}>
-      <SafeAreaView style={styles.safeArea}>
-        <ThemedView style={styles.heroSection}>
-          <AnimatedIcon />
-          <ThemedText type="title" style={styles.title}>
-            Welcome to&nbsp;Expo
-          </ThemedText>
-        </ThemedView>
-
-        <ThemedText type="code" style={styles.code}>
-          get started
-        </ThemedText>
-
-        <ThemedView type="backgroundElement" style={styles.stepContainer}>
-          <HintRow
-            title="Try editing"
-            hint={<ThemedText type="code">src/app/index.tsx</ThemedText>}
-          />
-          <HintRow title="Dev tools" hint={getDevMenuHint()} />
-          <HintRow
-            title="Fresh start"
-            hint={<ThemedText type="code">npm run reset-project</ThemedText>}
-          />
-        </ThemedView>
-
-        {Platform.OS === 'web' && <WebBadge />}
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#c092cc" }}>
+      <ScrollView contentContainerStyle= {styles.container}>
+        <View style= {styles.header}>
+          <Image
+            source={require("../../assets/images/avatar.jpg")} 
+            style= {styles.avatar}/>
+          <Text style={styles.nombre}>{nombre}</Text>
+          <Text style={styles.subtitulo}>Aprendiendo React Native</Text>
+        </View>
+        <View style={styles.card}>
+          <Text style={styles.label}>Nombre</Text>
+            <TextInput
+             value={nombre}
+             onChangeText={setNombre}
+             style={styles.input}
+             />
+            <Text style={styles.label}>Apellido</Text>
+              <TextInput
+                value={apellido}
+                onChangeText={setApellido}
+                style={styles.input}
+              />
+              <Text style={styles.label}>Mail</Text>
+              <TextInput
+                value={mail}
+                onChangeText={setMail}
+                style={styles.input}
+              />
+              <Text style={styles.label}>Biografía</Text>
+               <TextInput
+                    value={bio}
+                    onChangeText={setBio}
+                    style={styles.input}                    
+                    multiline
+               />                   
+        </View>
+        <TouchableOpacity style={styles.boton} onPress={()=> alert("Perfil actualizado")}>
+          <Text style={styles.botonTexto}> Guardar  </Text>
+        </TouchableOpacity>
+       </ScrollView>
       </SafeAreaView>
-    </ThemedView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    flexDirection: 'row',
-  },
-  safeArea: {
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    alignItems: 'center',
-    gap: Spacing.three,
-    paddingBottom: BottomTabInset + Spacing.three,
-    maxWidth: MaxContentWidth,
-  },
-  heroSection: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    gap: Spacing.four,
-  },
-  title: {
-    textAlign: 'center',
-  },
-  code: {
-    textTransform: 'uppercase',
-  },
-  stepContainer: {
-    gap: Spacing.three,
-    alignSelf: 'stretch',
-    paddingHorizontal: Spacing.three,
-    paddingVertical: Spacing.four,
-    borderRadius: Spacing.four,
-  },
-});
+const styles= StyleSheet.create({ 
+  container: {padding:20, alignItems:"center"},
+  header: {alignItems:"center",marginBottom:30},
+  avatar: {width:120,height: 120, borderRadius: 60},
+  nombre: {fontSize: 28, fontWeight: "bold", marginTop: 10},
+  subtitulo: { fontSize: 14, color: "#720f86",alignSelf: "center", fontStyle: "italic", marginTop: 4 },
+  card: {width:"100%", borderRadius: 8,backgroundColor: "#f9f9f9", padding:15},
+  label: {fontWeight:"600", marginBottom: 5,color:"#823d3d"},
+  input:{backgroundColor: "#fff", borderWidth: 1, borderColor:"#ddd", borderRadius:8, padding:10, fontSize:16},
+  boton: {backgroundColor: "#720f86", paddingVertical: 12, paddingHorizontal: 20, borderRadius: 8, alignItems: "center", justifyContent: "center", marginTop: 20,               
+    width: "100%"},    
+  botonTexto: {color: "#fff", textAlign: "center", fontWeight: "bold", fontSize: 16},
+})
